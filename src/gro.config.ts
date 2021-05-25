1c443ecc6df274f11b8bed1b437597e12070b3b9
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-05-24 18:18:28 -0600

    upgrade gro and remove unncessary config line (#26)

--- src/gro.config.ts
+++ src/gro.config.ts
@@ -2,7 +2,6 @@ import type {GroConfigCreator, GroConfigPartial} from '@feltcoop/gro';
 
 export const config: GroConfigCreator = async () => {
 	const partial: GroConfigPartial = {
-		builds: [{name: 'lib', platform: 'node', input: 'index.ts'}],
 		// by default, Gro bundles libraries, but Felt is published as individual modules
 		adapt: async () =>
 			(await import('@feltcoop/gro/dist/adapt/gro-adapter-node-library.js')).createAdapter({
