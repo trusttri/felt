968316169d36466e6ab1a957222858845ed3de90
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-05-25 13:40:33 -0600

    remove nav as public export (#27)

--- changelog.md
+++ changelog.md
@@ -1,5 +1,9 @@
 # changelog
 
+## 0.1.1
+
+- remove `Nav.svelte` from the public API
+
 ## 0.1.0
 
 - gogo
--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,4 @@ export * as icons from './lib/icons.js';
 // components
 export {default as FeltHeart} from './lib/FeltHeart.svelte';
 export {default as Icons} from './lib/Icons.svelte';
-export {default as Nav} from './lib/Nav.svelte';
 export {default as PendingAnimation} from './lib/PendingAnimation.svelte';
