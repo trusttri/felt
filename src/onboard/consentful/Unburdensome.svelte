937e4a899b914290d985ac1ff0f1bf9e5600fb10
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-06-05 17:39:42 -0600

    add content for `consentful-unburdensome` (#57)

--- src/onboard/consentful/Unburdensome.svelte
+++ src/onboard/consentful/Unburdensome.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import type {Onboard_Data} from '../onboard';
+	import Markup from '$lib/Markup.svelte';
 
 	export let data: Onboard_Data;
 	export let done: () => void;
 	export let back: () => void;
 </script>
 
-<button on:click={() => done()}> → </button>
+<Markup>
+	<h2>welcome to Felt.dev!</h2>
+</Markup>
+
+<blockquote>need help? <a href="https://www.felt.dev/help">felt.dev/help</a></blockquote>
+
+<button on:click={() => done()}>
+	<Markup>let's go →</Markup>
+</button>
---
+++ src/routes/help.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Help_Message from '$lib/Help_Message.svelte';
+</script>
+
+<div class="column">
+	<Help_Message text="todo" />
+	<h2>
+		<a href="https://github.com/feltcoop/felt/issues">github.com/feltcoop/felt/issues</a>
+	</h2>
+</div>
+
+<style>
+	.column {
+		margin: auto;
+	}
+	h2 {
+		padding: var(--spacing_lg) 0;
+		text-align: center;
+	}
+</style>
