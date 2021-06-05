98d8329f9baefc77014490385950174dfd089943
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-06-04 22:40:02 -0600

    add second confirmation diaglog to consentful revertible (#54)

--- src/onboard/consentful/Revertible.svelte
+++ src/onboard/consentful/Revertible.svelte
@@ -5,6 +5,8 @@
 	export let data: Onboard_Data;
 	export let done: () => void;
 	export let back: () => void;
+
+	let understood = false;
 </script>
 
 <Markup>
@@ -15,5 +17,12 @@
 	</p>
 	<button on:click={() => back()}>← go back and learn</button>
 	<blockquote>🌈✨ if you don't know the deal, it's not consentful✨✨</blockquote>
-	<button on:click={() => done()}>yes I understand →</button>
+	<button on:click={() => (understood = true)} disabled={understood}> yes I understand → </button>
+	{#if understood}
+		<p>
+			Here's <a href="/deal">a link to the documents</a> for the future, and you can always find
+			help in your <a href="/account">account settings</a>
+			<button class="inline" on:click={() => done()}> got it → </button>
+		</p>
+	{/if}
 </Markup>
