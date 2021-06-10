2b0a99931f1a43523919819a47795c3f68087d78
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-06-10 12:00:41 -0600

    remove markup wrapper (#74)
    @ryanatkn

--- src/lib/onboard/Credits.svelte
+++ src/lib/onboard/Credits.svelte
@@ -1,15 +1,9 @@
-<script lang="ts">
-	import Markup from '$lib/Markup.svelte';
-</script>
-
-<Markup>
-	<h3>TODO proper credits</h3>
-	<p>
-		This sketch is inspired by
-		<a href="https://consentful.systems"> consentful.systems </a> ✨
-	</p>
-	<p>
-		Open source permissive code at
-		<a href="https://github.com/feltcoop/felt"> github.com/feltcoop/felt </a> 💚
-	</p>
-</Markup>
+<h3>TODO proper credits</h3>
+<p>
+	This sketch is inspired by
+	<a href="https://consentful.systems"> consentful.systems </a> ✨
+</p>
+<p>
+	Open source permissive code at
+	<a href="https://github.com/feltcoop/felt"> github.com/feltcoop/felt </a> 💚
+</p>
