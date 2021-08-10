0c740e64c0e9949b26d39aac2fe4e01723f03d04
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-08-10 15:49:12 -0600

    add direct sketch routes instead of single slug (#117)

---
+++ src/routes/sketch/library.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Library from '$lib/sketch/library/Library.svelte';
+</script>
+
+<Library />
---
+++ src/routes/sketch/onboard.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Onboard from '$lib/sketch/onboard/Onboard.svelte';
+</script>
+
+<Onboard />
