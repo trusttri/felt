cbf5932ec03d1466ed8be54c4ec394c62e419cdb
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-08-12 10:12:35 -0500

    turn on immutable flag and update deps (#119)

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,9 @@ import staticAdapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: typescript(),
+	compilerOptions: {
+		immutable: true,
+	},
 	kit: {
 		adapter: staticAdapter(),
 		target: '#root',
