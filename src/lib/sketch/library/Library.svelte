20a5feea121ec2c1caad58ce505a00cda7356213
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-08-13 19:20:55 -0500

    library updates (#121)

--- src/lib/sketch/library/Library.svelte
+++ src/lib/sketch/library/Library.svelte
@@ -80,6 +80,36 @@
 	<hr />
 
 	<Markup>
+		<h2>
+			<code>form</code> <code>input</code> <code>textarea</code> <code>select</code>
+			<code>button</code>
+		</h2>
+		<form>
+			<input bind:value={username} disabled={created_account} placeholder="username" />
+			<input
+				type="password"
+				bind:value={password}
+				disabled={created_account}
+				placeholder="password"
+			/>
+			<textarea bind:value={lifestory} disabled={created_account} placeholder="life story" />
+			<select style="font-size: var(--font_size_xl5)" disabled={created_account}>
+				{#each faces as face (face)}
+					<option value={face}>{face}</option>
+				{/each}
+			</select>
+			<button type="button" disabled={created_account} on:click={() => create_account()}>
+				create account
+			</button>
+		</form>
+		{#if created_account}
+			<Message status="error">cannot create account because the library is fake</Message>
+		{/if}
+	</Markup>
+
+	<hr />
+
+	<Markup>
 		<h2><code>Checkbox</code></h2>
 	</Markup>
 	<div>
@@ -110,40 +140,10 @@
 		{/each}
 	</div>
 
-	<Markup>
-		<h2>
-			<code>form</code> <code>input</code> <code>textarea</code> <code>select</code>
-			<code>button</code>
-		</h2>
-	</Markup>
-	<Markup>
-		<form>
-			<input bind:value={username} disabled={created_account} placeholder="username" />
-			<input
-				type="password"
-				bind:value={password}
-				disabled={created_account}
-				placeholder="password"
-			/>
-			<textarea bind:value={lifestory} disabled={created_account} placeholder="life story" />
-			<select disabled={created_account}>
-				{#each faces as face (face)}
-					<option value={face}>{face}</option>
-				{/each}
-			</select>
-			<button type="button" disabled={created_account} on:click={() => create_account()}>
-				create account
-			</button>
-		</form>
-		{#if created_account}
-			<Message status="error">cannot create account because the library is fake</Message>
-		{/if}
-	</Markup>
+	<hr />
 
 	<Markup>
 		<h2><code>Message</code></h2>
-	</Markup>
-	<Markup>
 		<Message>:-)</Message>
 		<h3><code>Message</code> with <code>icon={'{'}face_expressionless{'}'}</code></h3>
 		<Message icon={face_expressionless}>cool and relevant info</Message>
@@ -162,6 +162,8 @@
 		<Message status="help" icon={item_key}>here take this</Message>
 	</Markup>
 
+	<hr />
+
 	<Markup>
 		<h2>typography</h2>
 	</Markup>
@@ -177,29 +179,29 @@
 			{/each}
 		</div>
 		<Markup>
-			<h1 style="color: {css_colors[0]}">h1</h1>
-			<h2 style="color: {css_colors[1]}">h2</h2>
-			<h3 style="color: {css_colors[2]}">h3</h3>
-			<h4 style="color: {css_colors[3]}">h4</h4>
-			<h5 style="color: {css_colors[4]}">h5</h5>
-			<h6 style="color: {css_colors[5]}">h6</h6>
-			<p style="font-size: var(--font_size_xs); color: {css_colors[6]}">
+			<h1 style="color: {css_colors[1]}">h1</h1>
+			<h2 style="color: {css_colors[2]}">h2</h2>
+			<h3 style="color: {css_colors[3]}">h3</h3>
+			<h4 style="color: {css_colors[4]}">h4</h4>
+			<h5 style="color: {css_colors[5]}">h5</h5>
+			<h6 style="color: {css_colors[6]}">h6</h6>
+			<p style="font-size: var(--font_size_xs); color: {css_colors[0]}">
 				--font_size_xs: 0.989rem;
 			</p>
-			<p style="font-size: var(--font_size_sm); color: {css_colors[6]}">
+			<p style="font-size: var(--font_size_sm); color: {css_colors[0]}">
 				--font_size_sm: 1.258rem;
 			</p>
-			<p style="font-size: var(--font_size_md); color: {css_colors[6]}">--font_size_md: 1.6rem;</p>
-			<p style="font-size: var(--font_size_lg); color: {css_colors[6]}">
+			<p style="font-size: var(--font_size_md); color: {css_colors[0]}">--font_size_md: 1.6rem;</p>
+			<p style="font-size: var(--font_size_lg); color: {css_colors[0]}">
 				--font_size_lg: 2.035rem;
 			</p>
-			<p style="font-size: var(--font_size_xl); color: {css_colors[6]}">
+			<p style="font-size: var(--font_size_xl); color: {css_colors[0]}">
 				--font_size_xl: 2.589rem;
 			</p>
-			<p style="font-size: var(--font_size_xl2); color: {css_colors[6]}">
+			<p style="font-size: var(--font_size_xl2); color: {css_colors[0]}">
 				--font_size_xl2: 3.293rem;
 			</p>
-			<p style="font-size: var(--font_size_xl3); color: {css_colors[6]}">
+			<p style="font-size: var(--font_size_xl3); color: {css_colors[0]}">
 				--font_size_xl3: 4.189rem;
 			</p>
 			<!-- <p style="font-size: var(--font_size_xl4);">--font_size_xl4: 5.328rem;</p>
@@ -221,22 +223,34 @@
 				{/each}
 			</div>
 			<div class="main-color">{main_color}</div>
+			<p class="xl-font-sizes">
+				<span style="font-size: var(--font_size_xl4); color: {css_colors[0]}"> xl4 </span>
+				<span style="font-size: var(--font_size_xl5); color: {css_colors[0]}"> xl5 </span>
+				<span style="font-size: var(--font_size_xl6); color: {css_colors[0]}"> xl6 </span>
+				<span style="font-size: var(--font_size_xl7); color: {css_colors[0]}"> xl7 </span>
+				<span style="font-size: var(--font_size_xl8); color: {css_colors[0]}"> xl8 </span>
+				<span style="font-size: var(--font_size_xl9); color: {css_colors[0]}"> xl9 </span>
+			</p>
 		</Markup>
 	</div>
+
+	<hr />
 
 	<Markup>
 		<h2><code>Icons</code></h2>
 	</Markup>
 	<Markup>
 		<h3>
-			<code>{'<'}span style="font-size: var(--...);"{'>'}🐢{'<'}/span{'>'}</code> with
-			<code>font-size</code>
-			set to
+			<code>{'<'}div{'>'}</code>s with width and height set to
 			<code>icon_size_xs</code>, <code>icon_size_sm</code>,
 			<code>icon_size_md</code>, <code>icon_size_lg</code>, <code>icon_size_xl</code>,
-			<code>icon_size_xl2</code>, <code>icon_size_xl3</code>, & the max <code>icon_size_xl4</code>,
-			which is <code>512px</code>
+			<code>icon_size_xl2</code>, <code>icon_size_xl3</code>, & the max <code>icon_size_xl4</code>
+			aka <code>icon_size_max</code> -- these are insensitive to browser font size, using
+			<code>px</code> not <code>rem</code>
 		</h3>
+		<blockquote>
+			these docs are currently lacking, so we suggest browser dev tools to improve the experience
+		</blockquote>
 		<div class="icon-sizes">
 			<div style="--size: var(--icon_size_xs);">🐢</div>
 			<div style="--size: var(--icon_size_sm);">🐢</div>
@@ -248,7 +262,12 @@
 			<div style="--size: var(--icon_size_xl4);">🐢</div>
 		</div>
 	</Markup>
+
+	<hr />
+
 	<Icons {icons} />
+
+	<hr />
 
 	<Markup>
 		<h2><code>Felt_Heart</code></h2>
@@ -259,6 +278,9 @@
 <style>
 	.typography {
 		display: flex;
+	}
+	hr {
+		margin: var(--spacing_xl5) 0;
 	}
 	.palette {
 		display: flex;
@@ -302,5 +324,8 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
+	}
+	.xl-font-sizes {
+		text-align: center;
 	}
 </style>
