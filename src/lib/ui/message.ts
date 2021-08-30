56937c0a68362ccf78ed5a9c5b535ac959dcbc59
trusttri <alwaysjane15@gmail.com>
2021-08-30 12:07:24 -0400

    fixed formatting issue. forgot a comma

--- src/lib/ui/message.ts
+++ src/lib/ui/message.ts
@@ -11,5 +11,5 @@ export const message_status_options: Record<MessageStatus, MessageStatusOptions>
 	normal: {color: 'var(--text_color_light)', icon: hand_thumbs_up},
 	help: {color: 'var(--help_color)', icon: hand_point_right},
 	error: {color: 'var(--error_color)', icon: hand_facing_forward},
-	inform: {color: 'var(--text_color_light)', icon: sparkle}
+	inform: {color: 'var(--text_color_light)', icon: sparkle},
 };
